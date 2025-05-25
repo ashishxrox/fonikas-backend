a6607b3d118acc163b56121feee8faa24fa89ce2
ashishxrox <ashishnair732@gmail.com>
2025-05-25 21:04:23 +0530

    Added start

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors({
   origin: ["http://localhost:5173", "https://fonikas.netlify.app"], // Allow frontend origin
